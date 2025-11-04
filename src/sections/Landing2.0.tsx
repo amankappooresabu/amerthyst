3023af09984602180a121bf045fb1a7897a47546
Aman <amansabu99@gmail.com>
2025-11-04 12:42:27 +0530

    Commit 21.0

--- src/sections/Landing2.0.tsx
+++ src/sections/Landing2.0.tsx
@@ -1,16 +1,16 @@
+import Spline from '@splinetool/react-spline';
+import '../styles/Landing.scss';
+
 export default function Landing1() {
     return (
-        <div className="w-full">
-            <video
-                autoPlay
-                loop={true}
-                muted
-                playsInline
-                className="object-contain "
-                style={{ zIndex: 1, width: '100%' }}
-            >
-                <source src="/video2.mp4" type="video/webm" />
-            </video>
+        <div className="landing-container">
+            <div className="spline-background">
+                <Spline
+                    scene="https://prod.spline.design/OlHtpmgzF5nZzi52/scene.splinecode" 
+                />
+            </div>
+            
+            
         </div>
     )
 }
